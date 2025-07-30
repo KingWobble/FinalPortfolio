7fa49dd8b0e1b19a1828f3ce99e5a34932fde8ea
KingWobble <jbernal@student.neumont.edu>
2025-07-30 16:33:53 -0600

    please work

--- theme-toggle.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector(".theme-toggle");
-
-  if (button) {
-    button.addEventListener("click", () => {
-      document.body.classList.toggle("light-mode");
-    });
-  }
-});
