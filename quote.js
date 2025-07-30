4fb1dfcb4bef25b5c4ab2f092e0c8a809ffae825
KingWobble <jbernal@student.neumont.edu>
2025-07-29 22:46:38 -0600

    Initial commit

---
+++ quote.js
@@ -0,0 +1,10 @@
+// js/quote.js
+fetch("https://zenquotes.io/api/random")
+  .then(res => res.json())
+  .then(data => {
+    const quote = data[0].q + " — " + data[0].a;
+    document.getElementById("quote").textContent = quote;
+  })
+  .catch(() => {
+    document.getElementById("quote").textContent = "Could not load quote.";
+  });
